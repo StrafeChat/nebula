f712ca50803a0f6f96abc65f8f20e5b4e30db2a9
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-01-18 00:12:44 -0600

    Fixes

--- src/routes/avatars.ts
+++ src/routes/avatars.ts
@@ -41,8 +41,8 @@ router.post<string, {}, {}, {}, {}, { user: User }>('/', verifyToken, async (_re
         await cassandra.execute(`
         UPDATE ${cassandra.keyspace}.users
         SET avatar=?
-        WHERE id=? AND created_at=?
-        `, [`${hashedAvatar}.webp`, res.locals.user.id, res.locals.user.created_at]);
+        WHERE id=?;
+        `, [`${hashedAvatar}.webp`, res.locals.user.id]);
 
         res.status(201).json({ avatar: `${hashedAvatar}.webp` });
     } catch (err) {
