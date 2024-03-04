d305e0e554f7cf7beb44916d1aeb516b23e5deaf
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-03-03 23:27:39 -0600

    github.com> Co-authored-by: Malik Whitten <MalikWhitten67@users.noreply.github.com> Co-authored-by: ShadowLp174 <shadowlp174@gmail.com>

--- src/routes/avatars.ts
+++ src/routes/avatars.ts
@@ -5,7 +5,9 @@ import path from "path";
 import sharp from 'sharp';
 import { avatarCount } from "..";
 import { cassandra } from "../database";
+import { decode } from "imagescript";
 import { verifyToken } from "../helpers/validator";
+import { createCanvas, loadImage } from 'canvas';
 import { User } from "../types";
 const router = Router();
 
@@ -38,11 +40,21 @@ router.post<string, {}, {}, {}, {}, { user: User }>('/', verifyToken, async (_re
         if (!fs.existsSync(avatarDir)) fs.mkdirSync(avatarDir);
         fs.writeFileSync(path.join(avatarDir, `${hashedAvatar}.webp`), buf);
 
+        let image = await decode(buf, true);
+        const accentInt = (image as any).averageColor();
+        
+        const red = (accentInt >> 24) & 0xFF;
+        const green = (accentInt >> 16) & 0xFF;
+        const blue = (accentInt >> 8) & 0xFF;
+        
+        const accentColor = (red << 16) | (green << 8) | blue;
+        console.log(accentColor)
+
         await cassandra.execute(`
         UPDATE ${cassandra.keyspace}.users
-        SET avatar=?
+        SET avatar=?, accent_color=?
         WHERE id=?;
-        `, [`${hashedAvatar}.webp`, res.locals.user.id]);
+        `, [`${hashedAvatar}.webp`, Number(accentColor), res.locals.user.id], { prepare: true });
 
         res.status(201).json({ avatar: `${hashedAvatar}.webp` });
     } catch (err) {
@@ -54,20 +66,43 @@ router.post<string, {}, {}, {}, {}, { user: User }>('/', verifyToken, async (_re
 router.patch<string, {}, {}, { data: string }, {}, { user: User }>('/', verifyToken, async (req, res) => {
     if (typeof req.body.data != "string") return res.status(400).json({ message: "Avatar data must be a base64 string." });
 
-    const regex = /^data:[a-z0-9]+\/[a-z0-9]+;base64,[a-zA-Z0-9+/=]+$/;
-    if (!regex.test(req.body.data)) return res.status(400).json({ message: "The data must be a valid base64 data url." });
+    // const regex = /^data:[a-z0-9]+\/[a-z0-9]+;base64,[a-zA-Z0-9+/=]+$/;
+    // if (!regex.test(req.body.data)) return res.status(400).json({ message: "The data must be a valid base64 data url." });
 
-    if (!res.locals.user.avatar) return res.status(409).json({ message: "You do not have an avatar setup. Did you mean to create one instead?" });
+    // if (!res.locals.user.avatar) return res.status(409).json({ message: "You do not have an avatar setup. Did you mean to create one instead?" });
 
     try {
         const base64 = req.body.data.split(',').pop();
+        console.log(base64)
         if (!base64) return res.status(400).json({ message: "The data must be a valid base64 data url." });
-        const buf = Buffer.from(base64, "base64");
+        const buffer = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+        const image = await loadImage(buffer);
+        const canvas = createCanvas(image.width, image.height);
+        const context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
 
+        const imageData = context.getImageData(0, 0, image.width, image.height).data;
+        let totalR = 0,
+          totalG = 0,
+          totalB = 0;
+    
+        for (let i = 0; i < imageData.length; i += 4) {
+          totalR += imageData[i];
+          totalG += imageData[i + 1];
+          totalB += imageData[i + 2];
+        }
+    
+        const numPixels = imageData.length / 4;
+        const [r, g, b] = [totalR / numPixels, totalG / numPixels, totalB / numPixels];
+        
+        function rgbToHexNumber(r: number, g: number, b: number): number {
+            return (r << 16) + (g << 8) + b;
+          }
+          console.log(rgbToHexNumber(r, g, b))
         let extension = '.webp';
 
         try {
-            const metadata = await sharp(buf).metadata();
+            const metadata = await sharp(buffer).metadata();
 
             if (metadata.pages! > 1) extension = '.gif';
         } catch (err) {
@@ -76,19 +111,19 @@ router.patch<string, {}, {}, { data: string }, {}, { user: User }>('/', verifyTo
         }
 
         const hash = crypto.createHash("sha256");
-        hash.update(buf);
+        hash.update(buffer);
         const hashedAvatar = `${hash.digest("hex")}${extension}`;
 
         const avatarDir = path.join("static", "avatars", res.locals.user.id);
 
         if (!fs.existsSync(avatarDir)) fs.mkdirSync(avatarDir);
-        await sharp(buf).toFile(path.join(avatarDir, `${hashedAvatar}${extension}`));
+        await sharp(buffer).toFile(path.join(avatarDir, `${hashedAvatar}`));
 
         await cassandra.execute(`
         UPDATE ${cassandra.keyspace}.users
         SET avatar=?
-        WHERE id=? AND created_at=?
-        `, [hashedAvatar, res.locals.user.id, res.locals.user.created_at]);
+        WHERE id=?
+        `, [hashedAvatar, res.locals.user.id]);
 
         res.status(201).json({ avatar: hashedAvatar });
     } catch (err) {
