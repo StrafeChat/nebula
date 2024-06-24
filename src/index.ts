eaa8aa17f474ca50c81982f8a00969bb44deb135
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-06-24 10:52:39 -0500

    I'm not smart I'm sorry

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ let avatarCount = 0;
 
 const app = express();
 
+app.use(bodyParser.json({ limit: '25mb' }));
 app.use(bodyParser.urlencoded({limit: "25mb", extended: true, parameterLimit:25000}));
 
 //app.use(helmet());
